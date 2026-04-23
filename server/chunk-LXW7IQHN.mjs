131128ef34260bb9f118937883a6f81100bc915b
castello-coffee <nasr.ahmed.0191@gmail.com>
2026-04-23 20:25:05 +0100

    Auto-generated commit

---
+++ server/chunk-LXW7IQHN.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+var e={production:!0,firebase:{apiKey:"AIzaSyByoVXkFoeVqv1qTYVek6tRgD_5ySq6Cj0",authDomain:"castello-menu-33228.firebaseapp.com",projectId:"castello-menu-33228",messagingSenderId:"677146910627",appId:"1:677146910627:web:f0db6efb05e19b7dd54416"},cloudinary:{cloudName:"db16it2kr",uploadPreset:"CASTELLO"}};export{e as a};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ZLXEHPTG.mjs";import"./chunk-E2OET7KW.mjs";import"./chunk-3JZHCBJS.mjs";import"./chunk-LXW7IQHN.mjs";import"./chunk-C36JLGKF.mjs";import"./chunk-ZJMROUUI.mjs";import"./chunk-RCP46FPH.mjs";import"./chunk-A45A2U7D.mjs";import"./chunk-3CVZQN5Z.mjs";export{a as default};
